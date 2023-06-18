0936e36fa9b315125c3ae13e9c64ba1a7fffb67b
EPAPRA <dave.devops109@gmail.com>
2023-06-18 21:55:08 +0530

    jenkins commit

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -10,7 +10,7 @@ def call() {
             stage('compile/build') {
                 steps {
                     script {
-                        common.compile
+                        common.compile()
                     }
                 }
             }
