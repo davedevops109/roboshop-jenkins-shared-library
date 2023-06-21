49b4f990328f7d7596b0170166de1fe4590c40a1
EPAPRA <dave.devops109@gmail.com>
2023-06-22 00:26:08 +0530

    jenkins commit

--- vars/common.groovy
+++ vars/common.groovy
@@ -13,16 +13,16 @@ def unittests() {
         //Developer is missing the test cases in our project, we need to add them as a best practice, we are skipping to proceed further
         try {
             sh 'npm test'
-    }
+        }
 
-    if (app_lang == "maven") {
-        sh 'mvn test'
-    }
+        if (app_lang == "maven") {
+            sh 'mvn test'
+        }
 
-    if (app_lang == "python") {
-        sh 'python3 -m unittest'
+        if (app_lang == "python") {
+            sh 'python3 -m unittest'
+        }
     }
-
 }
 
 def email (email_note) {
