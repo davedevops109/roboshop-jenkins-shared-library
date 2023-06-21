caaae096e9fb4b6e85397f6def8c8d23f975978a
EPAPRA <dave.devops109@gmail.com>
2023-06-21 23:41:33 +0530

    jenkins commit

--- vars/common.groovy
+++ vars/common.groovy
@@ -8,7 +8,7 @@ def compile() {
     }
 }
 
-def unittest() {
+def unittests() {
     if (app_lang == "nodejs") {
         //Developer is missing the test cases in our project, we need to add them as a best practice, we are skipping to proceed further
         //sh 'npm test'
