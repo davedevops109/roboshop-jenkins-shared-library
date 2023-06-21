4dbb40cc0dd4be5f6d3af319156f7eaf69ec1a53
EPAPRA <dave.devops109@gmail.com>
2023-06-22 01:52:21 +0530

    jenkins commit

--- vars/common.groovy
+++ vars/common.groovy
@@ -25,5 +25,6 @@ def unittests() {
 
 
 def email (email_note) {
-    mail bcc: '', body: 'Test\nNEW1\nNEW2', cc: '', from: 'shalem.david109@gmail.com', replyTo: '', subject: 'Test from Jenkins', to: 'shalem.david109@gmail.com'
+    sh 'env'
+    mail bcc: '', body: 'Job Failed', cc: '', from: 'shalem.david109@gmail.com', replyTo: '', subject: 'Test from Jenkins', to: 'shalem.david109@gmail.com'
 }
