9b931436f6e6d4536ab6852c97015aa331d8204e
EPAPRA <dave.devops109@gmail.com>
2023-06-23 00:48:37 +0530

    sonarqube commit

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -24,8 +24,14 @@ def call() {
                     }
                 }
                 stage('Quality control') {
+                    environment {
+                        SONAR_USER = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.user --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
+                        SONAR_PASS = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.pass --with-decryption --query Parameters[0].Value --with-decryption | sed \'s/"//g\')'
+                    }
                     steps {
-                        echo 'compile'
+
+                        sh 'sonar-scanner -Dsonar.host.url=http://184.73.104.208:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart'
+
                     }
                 }
                 stage('upload code to centralised place') {
