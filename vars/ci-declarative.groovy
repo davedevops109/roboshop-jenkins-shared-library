900265f14b6edb80d1043bf5e40c064ef6657258
EPAPRA <dave.devops109@gmail.com>
2023-06-24 19:13:36 +0530

    sonarqube commit

--- vars/ci-declarative.groovy
+++ vars/ci-declarative.groovy
@@ -1,27 +1,37 @@
 def call() {
     try {
-        node ('workstation') {
+        pipeline {
 
-            stage('CleanUp') {
-                cleanWs()
-            }
-            stage('compile/build') {
-                common.compile()
-            }
-
-            stage ('Unit Tests') {
-                common.unittests()
-            }
-
-            stage ('Quality control') {
-                SONAR_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
-                SONAR_USER = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.user --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
-                wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
-                    sh "sonar-scanner -Dsonar.host.url=http://52.6.208.71:9000/ -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
-            }
+        agent {
+            label 'workstation'
         }
-            stage('upload code to centralised place') {
-                echo 'upload'
+            stages {
+
+                stage('compile/build') {
+                    steps {
+                        script{
+                            common.compile()
+                        }
+                    }
+                }
+
+                stage('Unit Tests') {
+                    steps {
+                        script{
+                            common.unittests()
+                        }
+                    }
+                }
+                stage ('Quality control') {
+                    SONAR_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+                    SONAR_USER = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.user --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
+                    wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
+                        sh "sonar-scanner -Dsonar.host.url=http://52.6.208.71:9000/ -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
+                    }
+                }
+                stage('upload code to centralised place') {
+                    echo 'upload'
+                }
             }
         }
 
--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,7 +1,32 @@
 def call() {
-    node() {
-        stage('test') {
-            echo 'test'
+    try {
+        node ('workstation') {
+
+            stage('CleanUp') {
+                cleanWs()
+            }
+            stage('compile/build') {
+                common.compile()
+            }
+
+            stage ('Unit Tests') {
+                common.unittests()
+            }
+
+            stage ('Quality control') {
+                SONAR_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+                SONAR_USER = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.user --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
+                wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
+                    sh "sonar-scanner -Dsonar.host.url=http://52.6.208.71:9000/ -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
+                }
+            }
+            stage('upload code to centralised place') {
+                echo 'upload'
+            }
         }
+
+
+    }   catch(Exception e) {
+        common.email ("failed")
     }
 }
