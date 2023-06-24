83103cb93f9b14b28821d0972a69f2bba4387f99
EPAPRA <dave.devops109@gmail.com>
2023-06-24 23:51:10 +0530

    sonarqube commit

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,4 +1,8 @@
 def call() {
+
+    if(!env.SONAR_EXTRA_OPTS) {
+        env.SONAR_EXTRA_OPTS = " "
+    }
     try {
         node('workstation') {
 
@@ -16,7 +20,7 @@ def call() {
 
             stage ('Quality control') {
                 SONAR_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
-                SONAR_USER = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.user --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
+                SONAR_USER = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.user --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
                 wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
                     sh "sonar-scanner -Dsonar.host.url=http://52.6.208.71:9000/ -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=${component} ${SONAR_EXTRA_OPTS}"
                 }
