3af425530a365037b234e4ff640a342acea9f70e
EPAPRA <dave.devops109@gmail.com>
2023-06-24 17:59:10 +0530

    sonarqube commit

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -30,8 +30,10 @@ def call() {
                     }
                     steps {
                         script {
+                            SONAR_PASS = sh (script: aws ssm get-parameters --region us-east-1 --names sonarqube.pass --with-decryption --query Parameters[0].Value | sed 's/"//g', returnStdout: true).trim()
                             wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
                                 println "PASSWORD = ${SONAR_PASS}"
+                                sh "echo sh password = ${SONAR_PASS}"
                                 sh "sonar-scanner -Dsonar.host.url=http://52.6.208.71:9000/ -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
                             }
                         }
