709920849b704b8cdb5789024c69369353f627d1
EPAPRA <dave.devops109@gmail.com>
2023-06-30 13:12:52 +0530

    multibranch commit

--- vars/common.groovy
+++ vars/common.groovy
@@ -29,21 +29,20 @@ def email (email_note) {
     mail bcc: '', body: "Job Failed - ${JOB_BASE_NAME}\nJenkins URL - ${JOB_URL}", cc: '', from: 'shalem.david109@gmail.com', replyTo: '', subject: "Jenkins Job failed - ${JOB_BASE_NAME}", to: 'shalem.david109@gmail.com'
 }
 
-def artifactPush(){
+def artifactPush() {
     sh "echo ${TAG_NAME} >VERSION"
 
     if (app_lang == "nodejs") {
-       sh "zip -r ${component}-${TAG_NAME}.zip node_modules server.js VERSION"
+        sh "zip -r ${component}-${TAG_NAME}.zip node_modules server.js VERSION"
     }
     sh 'ls -l'
 
-    }
     if (app_lang == "nginx" || app_lang == "python") {
         sh "zip -r ${component}-${TAG_NAME}.zip * -x Jenkinsfile"
     }
 
-    NEXUS_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names nexus.pass --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
-    NEXUS_USER = sh ( script: 'aws ssm get-parameters --region us-east-1 --names nexus.user --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+    NEXUS_PASS = sh(script: 'aws ssm get-parameters --region us-east-1 --names nexus.pass --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+    NEXUS_USER = sh(script: 'aws ssm get-parameters --region us-east-1 --names nexus.user --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
     wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${NEXUS_PASS}", var: 'SECRET']]]) {
         sh "curl -v -u ${NEXUS_USER}:${NEXUS_PASS} --upload-file ${component}-${TAG_NAME}.zip http://172.31.88.190:8081/repository/${component}/${component}-${TAG_NAME}.zip"
 
